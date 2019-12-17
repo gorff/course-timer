9399174867271ac3b2219a958c16d163c7661e5d
gorff <mcdojj12@gmail.com>
2019-12-17 10:40:06 +1000

    added functionality to project how much of the course you should be done by a certain date

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-
-
 // Get current date
-
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var dueDate = document.getElementById("daysLeft").innerHTML;
 
 // Listen for a change in the date input
 document.getElementById("dateInput").addEventListener("change", function() {
-    var input = this.value;
-    var dateEntered = new Date(input);
-    var currentDate = new Date(date);
+  var input = this.value;
+  var dateEntered = new Date(input);
+  var currentDate = new Date(date);
 
-    const diffTime = Math.abs(dateEntered - currentDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffTime = Math.abs(dateEntered - currentDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-    var daysLeft = dateEntered - currentDate;
+  var daysLeft = dateEntered - currentDate;
 
-    // Set the output text to show the days left
-    document.getElementById("daysLeft").innerHTML = diffDays;
+  // Set the output text to show the days left
+  document.getElementById("daysLeft").innerHTML = diffDays;
 });
-  document.getElementById("percentDone").addEventListener("change", function() {
 
-    // Percent done section
-    var percentCompleted = document.getElementById("percentDone").value;
-    const diffDays = document.getElementById("daysLeft").innerHTML // need to redefine, as its initially declared inside another function as a local var
-    amountEachDay = (100 - percentCompleted)/ diffDays;
+// Percent done section
+document.getElementById("percentDone").addEventListener("change", function() {
+  var percentCompleted = document.getElementById("percentDone").value;
+  const diffDays = document.getElementById("daysLeft").innerHTML // need to redefine, as its initially declared inside another function as a local var
+  amountEachDay = (100 - percentCompleted) / diffDays;
 
-    console.log("% completed logged")
+  console.log("% completed logged")
 
-    document.getElementById("amountPerDay").innerHTML = Math.round(amountEachDay*100)/100 +'%';
+  document.getElementById("amountPerDay").innerHTML = Math.round(amountEachDay * 100) / 100 + '%';
+});
 
+// Amount to be completed by a specified date section
+document.getElementById("futureDate").addEventListener("change", function() {
+  // Get values from previous results
+  var percentCompleted = parseFloat(document.getElementById("percentDone").value);
+  var amountEachDay =  parseFloat(document.getElementById("amountPerDay").innerHTML.replace('%',  ''));
+  var currentDate = new Date(date);
+  var futureDate = new Date(document.getElementById("futureDate").value);
+  // EQUATION:
+  // amount done by date = amountCompleted + %perDay * (futureDate - currentDate)
+  const diffTime = Math.abs(futureDate - currentDate);
+  const futureDiffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  var futureAmount = Math.round((percentCompleted + futureDiffDays*amountEachDay)*100)/100;
 
-
+  // Display result
+  document.getElementById("futureValue").innerHTML = futureAmount + '%';
 });
